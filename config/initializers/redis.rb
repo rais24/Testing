cf8d3d673568a4a9dc6bb299b6e8df1cfd45654c
rais24 <rishirais24@gmail.com>
2015-03-24 22:19:48 +0530

    Regis changed

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,5 @@
+ENV["REDISTOGO_URL"] ||= "redis://redistogo:91fd76a157183c1fb7346c02d3488a00@greeneye.redistogo.com:11535/"
+
 uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
+
